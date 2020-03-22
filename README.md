5a726317623ec6db65ba1a3277815e60ffb98654
Rudy Faile <febesbrasil@gmail.com>
2020-03-22 11:45:32 -0500

    pt2

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Abstract-Scraper
 A python script for scraping abstract data from journal sites
+![Settingsimage](/images/settingsScreen.png)
--- main.py
+++ main.py
@@ -1,4 +1,15 @@
+from settingsGUI import settings_menu
+from requestLogic import search_sort
 
+VERSION='1.0'
+
+
+
+
+def main():
+    event, value = settings_menu(VERSION)
+    print(event, value)
+    search_sort(value[0], value[1], value[2], value[3], value[4], value[5], value[6], value[7], value[8], value[9], value[10])
+    
 if __name__ == '__main__':
-    # execute only if run as the entry point into the program
     main()
